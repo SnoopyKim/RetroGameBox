47c41eaf3393117f70e27f0a333a5f750ce912b5
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2022-01-01 10:33:57 +0900

    RGB start

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+let backgroundImg = require("./bg_images/main_bg.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.backgroundImage} source={backgroundImg} />
+      <StatusBar style="light" />
+      <View>
+        <Button title="cheese"></Button>
+      </View>
     </View>
   );
 }
@@ -13,8 +20,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  backgroundImage: {
+    flex: 1,
+    alignSelf: `stretch`,
+    width: null,
   },
 });
