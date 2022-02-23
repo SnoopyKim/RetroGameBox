dc16b773d0880037808b1bb1cac2cb5779c09d72
312312312312312312312 <89693278+312312312312312312312@users.noreply.github.com>
2022-02-23 23:13:37 +0900

    JumpGame Btn add

--- games/JumpGame/index.jsx
+++ games/JumpGame/index.jsx
@@ -10,10 +10,12 @@ import {
   Text,
   ImageBackground,
   TouchableOpacity,
+  Image,
   View,
 } from "react-native";
 import Constants from "./Constants";
 import Floor from "./components/Floor";
+import Player from "./components/Player";
 import { GameEngine } from "react-native-game-engine";
 import AssetLoading from "../../components/AssetLoading";
 
@@ -32,19 +34,59 @@ const JumpGameScreen = () => {
 
     let floor = Matter.Bodies.rectangle(
       Constants.MAX_WIDTH / 2,
-      Constants.MAX_HEIGHT / 4,
+      Constants.MAX_HEIGHT / 1.45 - 25,
       Constants.MAX_WIDTH,
-      Constants.MAX_HEIGHT / 4,
+      50,
       { isStatic: true }
     );
+    let floor2 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      floor.position.y - 170,
+      Constants.MAX_WIDTH,
+      20,
+      { isStatic: true }
+    );
+    let floor3 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      floor2.position.y - 170,
+      Constants.MAX_WIDTH,
+      20,
+      { isStatic: true }
+    );
+    let player = Matter.Bodies.rectangle(40, floor.position.y - 50, 40, 40, {
+      isStatic: true,
+    });
 
-    Matter.World.add(matterEngine.current.world, [floor]);
+    Matter.World.add(matterEngine.current.world, [
+      floor,
+      floor2,
+      floor3,
+      player,
+    ]);
     return {
       floor: {
         body: floor,
-        size: [Constants.MAX_WIDTH, Constants.MAX_HEIGHT / 4],
+        size: [Constants.MAX_WIDTH, 50],
+        color: "#964b00",
+        renderer: Floor,
+      },
+      floor2: {
+        body: floor2,
+        size: [Constants.MAX_WIDTH - 90, 20],
         color: "purple",
         renderer: Floor,
+      },
+      floor3: {
+        body: floor3,
+        size: [Constants.MAX_WIDTH - 80, 20],
+        color: "purple",
+        renderer: Floor,
+      },
+      player: {
+        body: player,
+        size: [50, 50],
+        color: "red",
+        renderer: Player,
       },
     };
   };
@@ -69,7 +111,33 @@ const JumpGameScreen = () => {
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
-            <Text>Time</Text>
+            <TouchableOpacity>
+              <View>
+                <Image
+                  source={require("../../assets/images/redBtn.png")}
+                  resizeMode="contain"
+                  style={{ width: 120, height: 120 }}
+                ></Image>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View>
+                <Image
+                  source={require("../../assets/images/redBtn.png")}
+                  resizeMode="contain"
+                  style={{ width: 120, height: 120 }}
+                ></Image>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View>
+                <Image
+                  source={require("../../assets/images/redBtn.png")}
+                  resizeMode="contain"
+                  style={{ width: 120, height: 120 }}
+                ></Image>
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
       </ImageBackground>
@@ -99,7 +167,7 @@ const styles = StyleSheet.create({
   controls: {
     flex: 1,
     width: Constants.MAX_WIDTH,
-    height: 100,
+    height: Constants.MAX_HEIGHT / 4,
     position: "absolute",
     backgroundColor: "black",
     top: Constants.MAX_HEIGHT / 1.45,
